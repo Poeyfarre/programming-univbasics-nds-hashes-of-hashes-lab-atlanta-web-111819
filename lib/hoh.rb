65b5fd7e6d96e5b2cd3d54134cb23516f6761240
Jacy Leech <jacy.atlanta@gmail.com>
2019-11-14 01:14:57 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -5,7 +5,27 @@
 # The tests will guide your construction.
 #
 PORTION_1 = {
-  
+  {label:"Kingdom",
+    sub_category:{
+      label:"Phylum",
+        sub_category:{
+          label:"Class",
+            sub_category:{
+              label:"Order",
+                sub_category:{
+                  label:"Family",
+                    sub_category:{
+                      label:"Genus",
+                        sub_category:{
+                          label:"Species"
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 
 def naming_system
   # Remember:
